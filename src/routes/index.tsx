c4051444240921ba1446ed4cd9c228824a5b5516
Ewen Hill <ewen.hill@gmail.com>
2022-12-30 22:42:19 +1300

    Update Index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+//import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
+      <!--<Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
       <p>
@@ -13,7 +13,7 @@ export default function Home() {
           start.solidjs.com
         </a>{" "}
         to learn how to build SolidStart apps.
-      </p>
+      </p>-->
       <iframe class="mj-w-res-iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://app.mailjet.com/widget/iframe/8Sp8/P5q" width="100%"></iframe>
 
 <script type="text/javascript" src="https://app.mailjet.com/statics/js/iframeResizer.min.js"></script>
